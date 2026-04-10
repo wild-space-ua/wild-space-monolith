5ea44484506122170e6d05b105fdbbe46b1cb0ed
Iced-Coded <volkogon212@gmail.com>
2026-04-10 21:57:49 +0300

    Add logo

--- Content.Client/MainMenu/UI/MainMenuControl.xaml.cs
+++ Content.Client/MainMenu/UI/MainMenuControl.xaml.cs
@@ -22,7 +22,7 @@ public sealed partial class MainMenuControl : Control
         LayoutContainer.SetMarginTop(VBox, 30);
         LayoutContainer.SetGrowHorizontal(VBox, LayoutContainer.GrowDirection.Begin);
 
-        var logoTexture = resCache.GetResource<TextureResource>("/Textures/_Mono/Logo/logo.png"); // Frontier: add _NF
+        var logoTexture = resCache.GetResource<TextureResource>("/Textures/_WildSpace/Logo/logo.png");
         Logo.Texture = logoTexture;
 
         var currentUserName = configMan.GetCVar(CVars.PlayerName);
